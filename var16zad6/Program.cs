73668ac9da6ffbf7485d2460cb4bc01e2e860dde
Strange <nazaroff.serezha2014@gmail.com>
2021-05-17 09:20:32 +0300

    Добавьте файлы проекта.

---
+++ var16zad6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace var16zad6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bezlemith bezlemith = new bezlemith(100, "Doh");
+            MinuteTariff minuteTariff = new MinuteTariff("Nih", 10);
+            Console.WriteLine(bezlemith.PrintInfo() + "\n" + minuteTariff.PrintInfo());
+        }
+    }
+}
